a3d7af26fc049d3c8b577a6fbf9b2b9e420517d5
kosugue <kosugue2@yahoo.com.br>
2026-01-29 20:43:15 -0400

    sicronização commit do IntelliJ Artistrepository

--- src/main/java/com/seplagseletivo/projeto_backend/repository/ArtistRepository.java
+++ src/main/java/com/seplagseletivo/projeto_backend/repository/ArtistRepository.java
@@ -3,6 +3,7 @@ package com.seplagseletivo.projeto_backend.repository;
 import com.seplagseletivo.projeto_backend.entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
