52f9b43a534b46a34142be14d9e7ddd0ed64b46a
kosugue <kosugue2@yahoo.com.br>
2026-01-29 20:44:35 -0400

    sicronização commit do IntelliJ MusicApi

--- src/main/java/com/seplagseletivo/projeto_backend/service/MusicApiApplication.java
+++ src/main/java/com/seplagseletivo/projeto_backend/service/MusicApiApplication.java
@@ -1,4 +1,4 @@
-package com.seplagseletivo.projeto_backend;
+package com.seplagseletivo.projeto_backend.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
