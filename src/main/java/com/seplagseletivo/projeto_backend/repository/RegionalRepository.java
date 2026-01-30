6ddf9311ef97b88bad3cb3ba05b28d1e169374f8
kosugue <kosugue2@yahoo.com.br>
2026-01-29 20:38:22 -0400

    adicionando classes regional e user

---
+++ src/main/java/com/seplagseletivo/projeto_backend/repository/RegionalRepository.java
@@ -0,0 +1,15 @@
+package com.seplagseletivo.projeto_backend.repository;
+
+import com.seplagseletivo.projeto_backend.entity.Regional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RegionalRepository extends JpaRepository<Regional, Long> {
+    // Busca apenas as regionais que estão ativas no momento
+    List<Regional> findByAtivoTrue();
+
+    // Busca regional ativa por ID original (da API)
+    Optional<Regional> findByOriginalIdAndAtivoTrue(Long originalId);
+}
