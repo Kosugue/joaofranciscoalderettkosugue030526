ac0cb501626941e6a64611e6eaa08701aab5aa97
kosugue <kosugue2@yahoo.com.br>
2026-01-29 20:34:21 -0400

    DTO request, response regional

---
+++ src/main/java/com/seplagseletivo/projeto_backend/dto/RegionalDTO.java
@@ -0,0 +1,12 @@
+package com.seplagseletivo.projeto_backend.dto;
+
+public class RegionalDTO {
+    private Long id;
+    private String nome;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
